f42d525fb2156c0f26676c83be0fe1d229f0bfda
Mike Litman <awesomefat@gmail.com>
2021-03-22 14:00:48 -0500

    made changes

--- rot13Messenger/reader.c
+++ rot13Messenger/reader.c
@@ -11,7 +11,7 @@ int main()
     int shmid = shmget(key,1024,0666|IPC_CREAT); 
   
     // shmat to attach to shared memory 
-    char *str = (char*) shmat(shmid,(void*)0,0); 
+    char* str = (char*) shmat(shmid,(void*)0,0); 
   
     printf("Data read from memory: %s\n",str); 
       
--- rot13Messenger/writer.c
+++ rot13Messenger/writer.c
@@ -6,9 +6,18 @@ int main()
 { 
     // ftok to generate unique key 
     key_t key = ftok("shmfile",65); 
-  
+
+    printf("%d\n", IPC_CREAT);
     // shmget returns an identifier in shmid 
     int shmid = shmget(key,1024,0666|IPC_CREAT); 
+
+    /*
+    0666 | 512 = MASKING
+    1010011010
+    1000000000
+    1010011010 = 66610
+    */
+
   
     // shmat to attach to shared memory 
     char *str = (char*) shmat(shmid,(void*)0,0); 
